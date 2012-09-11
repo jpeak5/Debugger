985f5b6d1cd8f431d0efdf3f659d7cb218766cad
jason peak <jpeak5@lsu.edu>
2012-09-11 18:19:38 -0500

    yep this is fine...

--- Debugger.php
+++ Debugger.php
@@ -1,12 +1,26 @@
 <?php
+
 class Debugger {
 
-public $active = true;
+    const DEBUG = true;
 
-public function blast(){
+    public static function blast($msg, $args = null, $die = false) {
+
+        if (Debugger::DEBUG) {
+            if ($args) {
+                $fargs = func_get_args();
+                if (!is_array($fargs[1])) {
+                    echo sprintf($msg, $args);
+                } else {
+                    echo vsprintf($msg, $args);
+                }
+            }else{
+                echo $msg;
+            }
+            if($die){
+                die("<hr/>Execution killed by ".get_class()."<hr/>");
+            }
+        }
+    }
 
 }
-
-
-}
-$debugger = new Debugger();
